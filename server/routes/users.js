5d2b6c566a0c769b2eb214d43f03e516e0318d42
Naveen Sai <spnaveen675@gmail.com>
2022-05-09 10:34:36 +0530

    Added Update logic in server

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,23 +1,77 @@
 const express = require('express');
 const router = express.Router();
 const UserCredDb = require('../models/userCredentials')
+const User = require('../models/users');
 router.use(express.json());
 
 
-router.post('/', (req,res) => {
+router.post('/user', (req,res) => {
 
    
 
     const data = req.body;
-    const {username,password} = data;
+    const {username,password,firstname,lastname,gmail} = data;
 
     const credentials = new UserCredDb({
         'username' : username,
         'password' : password
     })
-    credentials.save().then(() => {res.status(201)}).catch((err) => {
-        res.status(500).send(err)
-    })
+    credentials.save().then(() => {
+       
+        UserCredDb.findOne({'username' : username}).then((data) => {
+
+            const userId = data._id;
+            console.log(userId);
+            const newEntry = new User({
+                 'id' : userId,
+                 'username' : username,
+                 'firstname' : firstname,
+                 'lastname' : lastname,
+                 'gmail' : gmail
+
+             })
+
+             newEntry.save().then(() => {res.status(201).send()} )
+        }).catch((err) => {
+            res.status(500).send(err);
+         })
+    }
+    )
+
+})
+
+router.put('/me', (req,res) => {
+
+    const data = req.body;
+    const {firstname,lastname,gmail,id} = data;
+
+
+    if(! firstname){
+        if(! lastname){
+            User.findOneAndUpdate(({'id' : id}, {'gmail' : gmail})).then(() => {res.status(204).send('update Completed')}).catch((err) => {res.status(500).send(err)});
+        }
+        else{
+            User.findOneAndUpdate(({'id' : id}, {'gmail' : gmail, 'lastname' : lastname})).then(() => {res.status(204).send('update Completed')}).catch((err) => {res.status(500).send(err)});
+        }
+    }
+    else if(! lastname){
+
+        if(! gmail){
+            
+            User.findOneAndUpdate(({'id' : id}, {'firstname':firstname})).then(() => {res.status(204).send('update Completed')}).catch((err) => {res.status(500).send(err)});
+        }
+        else{
+            User.findOneAndUpdate(({'id' : id}, {'firstname':firstname, 'gmail': gmail})).then(() => {res.status(204).send('update Completed')}).catch((err) => {res.status(500).send(err)});
+        }
+        
+    }
+    else if(! gmail){
+
+        User.findOneAndUpdate(({'id' : id}, {'firstname':firstname , 'lastname' : lastname})).then(() => {res.status(204).send('update Completed')}).catch((err) => {res.status(500).send(err)});
+    }
+    else{
+        User.findOneAndUpdate(({'id' : id}, {'firstname':firstname , 'lastname': lastname ,'gmail' : gmail})).then(() => {res.status(204).send('update Completed')}).catch((err) => {res.status(500).send(err)});
+    }
 
 })
 
