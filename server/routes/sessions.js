c56682f9add3852b7a432f9453c176fd72bab36e
Naveen Sai <38736123+naveen675@users.noreply.github.com>
2023-12-05 17:48:48 +0530

    Update sessions.js

--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 router.use(express.json());
 const auth = require('../middleware/auth');
-
+//auth
 
 router.delete('/me', (req,res) => {
 
